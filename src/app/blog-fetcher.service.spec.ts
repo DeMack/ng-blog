83879cc12d1c541dbd52cf8b1cf9de86b826024c
DanielMc <daniel_mckerrocher@ultimatesoftware.com>
2018-03-30 16:44:30 -0400

    set up blog fetcher tests

--- src/app/blog-fetcher.service.spec.ts
+++ src/app/blog-fetcher.service.spec.ts
@@ -1,15 +1,33 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { BlogFetcherService } from './blog-fetcher.service';
+import { Blog } from './blog';
 
 describe('BlogFetcherService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
       providers: [BlogFetcherService]
+
     });
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', inject([BlogFetcherService], (service: BlogFetcherService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('#getBlogs should return value from observable', inject([ BlogFetcherService ], (service: BlogFetcherService) => {
+    service.getBlogs().subscribe(value => {
+      const blog = {id: 'id1', title: 'TestPost', body: 'Test body', date: new Date(), tags: ['testTag1', 'testTag2'] };
+      expect(value).toEqual(jasmine.arrayContaining([blog]));
+    });
   }));
 });
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -5,8 +5,8 @@ export class InMemoryDataService implements InMemoryDbService {
 
   createDb() {
     const blogs = [
-      { id: UUID(), title: 'TestPost', body: 'Test body', date: new Date(), tags: ['testTag1', 'testTag2'] },
-      { id: UUID(), title: 'TestPost2', body: 'Test body 2', date: new Date(), tags: ['testTag1', 'testTag3'] }
+      { id: 'id1', title: 'TestPost', body: 'Test body', date: new Date(), tags: ['testTag1', 'testTag2'] },
+      { id: 'id2', title: 'TestPost2', body: 'Test body 2', date: new Date(), tags: ['testTag1', 'testTag3'] }
     ];
     return blogs;
   }
